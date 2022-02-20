6ce1aeb95646276b28423df75399552d8722772b
KelaKing <jxwywxy@icloud.com>
2022-02-20 16:41:23 +0800

    SelectState

--- SudokuPaper/ViewController.swift
+++ SudokuPaper/ViewController.swift
@@ -7,9 +7,15 @@
 
 import Cocoa
 
+enum SelectState {
+    case none
+    case selected(row: Int, column: Int)
+}
+
 class ViewController: NSViewController {
     
     let sudokuView = SudokuView()
+    var selectState = SelectState.none
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +29,23 @@ class ViewController: NSViewController {
     }
 
     override func mouseDown(with event: NSEvent) {
-        print("mouse")
+        let point = view.convert(event.locationInWindow, to: sudokuView)
+        
+        let column = Int(point.x.rounded()) / Int(cellSize)
+        let row = 8 - Int(point.y.rounded()) / Int(cellSize)
+        
+        switch selectState {
+        case .none:
+            selectState = .selected(row: row, column: column)
+        case .selected(let oldRow, let oldColumn):
+            if row == oldRow && column == oldColumn {
+                selectState = .none
+            } else {
+                selectState = .selected(row: row, column: column)
+            }
+        }
+        
+        print("New state: \(selectState)")
     }
 
     override func keyDown(with event: NSEvent) {
